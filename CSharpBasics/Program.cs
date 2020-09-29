f35371fc9fb5ccd8275036aad5bb5ce505ea5f8a
Your Name <ansabaj5@gmail.com>
2020-09-29 09:43:20 +0545

    Added CSharpBasics project

---
+++ CSharpBasics/Program.cs
@@ -0,0 +1,22 @@
+﻿// using System;
+
+// namespace CSharpBasics
+// {
+//     class Program
+//     {
+//         static void Main()
+//         {
+//             SelectionStatement selectionStatement = new SelectionStatement();
+//             selectionStatement.LearnIfElse();
+//             selectionStatement.LearnSwitchCases();
+//         }
+//     }
+// }
+class Program
+{
+    static void Main123()
+    {
+        IterationStatement iterationStatement = new IterationStatement();
+        iterationStatement.LearnForLoops();
+    }
+}
